8b4bdea5d81b6f87bf676249b6a497afadefa601
PoojaB26 <pbhaumik26@gmail.com>
2017-12-29 02:35:58 +0530

    Re init

---
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.android.waitlist;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private GuestListAdapter mAdapter;
+    private SQLiteDatabase mDb;
+    private EditText etNewGuestName, etNewPartySize;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView waitlistRecyclerView;
+
+        // Set local attributes to corresponding views
+        waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
+
+        etNewGuestName = (EditText) findViewById(R.id.person_name_edit_text);
+        etNewPartySize = (EditText) findViewById(R.id. party_count_edit_text);
+        // Set layout for the RecyclerView, because it's a list we are using the linear layout
+        waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        // Create a DB helper (this will create the DB if run for the first time)
+        WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
+
+        // Keep a reference to the mDb until paused or killed. Get a writable database
+        // because you will be adding restaurant customers
+        mDb = dbHelper.getWritableDatabase();
+
+      //  TestUtil.insertFakeData(mDb);
+
+        // Get all guest info from the database and save in a cursor
+        Cursor cursor = getAllGuests();
+
+        // Create an adapter for that cursor to display the data
+        mAdapter = new GuestListAdapter(this, cursor);
+
+        // Link the adapter to the RecyclerView
+        waitlistRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    /**
+     * This method is called when user clicks on the Add to waitlist button
+     *
+     * @param view The calling view (button)
+     */
+    public void addToWaitlist(View view) {
+
+        if(etNewPartySize.length() == 0 || etNewPartySize.length() == 0)
+            return;
+
+
+        int partySize = 1;
+        try {
+            partySize = Integer.parseInt(etNewPartySize.getText().toString());
+        }   catch(Exception e){
+
+        }
+
+        addNewGuest(etNewGuestName.getText().toString(), partySize);
+        mAdapter.swapCursor(getAllGuests());
+        etNewPartySize.getText().clear();
+        etNewGuestName.getText().clear();
+        etNewPartySize.clearFocus();
+
+
+        // TODO (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
+
+    }
+
+
+
+    /**
+     * Query the mDb and get all guests from the waitlist table
+     *
+     * @return Cursor containing the list of guests
+     */
+    private Cursor getAllGuests() {
+        return mDb.query(
+                WaitlistContract.WaitlistEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
+        );
+    }
+
+    private long addNewGuest(String name, int size){
+        ContentValues cv = new ContentValues();
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE, size);
+        return mDb.insert(WaitlistContract.WaitlistEntry.TABLE_NAME, null, cv);
+    }
+
+
+
+
+
+}
